5f68ed42a801476bee62bcaccb9ed1c91fc69463
proxy <app@nidaye.us>
2021-08-13 13:52:00 +0800

    update

--- Scripts/imchat01.js
+++ Scripts/imchat01.js
@@ -2,6 +2,6 @@ let obj = JSON.parse($response.body);
 obj.d.vip = "1";
 obj.d.age = "37";
 obj.d.city = "北京";
-obj.d.birthday = "1900\/01\/01";
+obj.d.birthday = "1984\/01\/01";
 obj.d.score = "233";
 $done({ body: JSON.stringify(obj) });
