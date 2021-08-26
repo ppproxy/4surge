2bab52e32c4eee902e62663d964f1a959e7e17f7
pproxy <5180305+ppproxy@users.noreply.github.com>
2021-08-26 17:55:51 +0800

    Update xbl01.js

--- Scripts/xbl01.js
+++ Scripts/xbl01.js
@@ -1,10 +1,9 @@
 console.log($response);
-
 if ($request.method != "POST" || $response.status != 200) {
   $done({});
 } else {
   let obj = JSON.parse($response.body);
   obj.data.qrColor = "GREEN";
   obj.data.lastScanCity = "北京";
-$done({ body: JSON.stringify(obj) });
+  $done({ body: JSON.stringify(obj) });
 }
