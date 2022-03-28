aab2d050d618cfe067d20a2b1ca4948f90016a44
pproxy <5180305+ppproxy@users.noreply.github.com>
2022-03-28 17:18:41 +0800

    Update xxt01.js

--- Scripts/xxt01.js
+++ Scripts/xxt01.js
@@ -2,6 +2,6 @@ if ($request.method != "POST" || $response.status != 200) {
   $done({});
 } else {
   let obj = JSON.parse($response.body);
-  obj.cumulateHour = parseFloat(obj.cumulateHour)+1000;
+  obj.cumulateHour = parseFloat($obj.cumulateHour)+1000;
   $done({ body: JSON.stringify(obj) });
 }
