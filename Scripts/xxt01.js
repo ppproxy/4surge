d663aa2f22d16761dbf5896fccd3a909017782d3
pproxy <5180305+ppproxy@users.noreply.github.com>
2022-03-30 16:22:48 +0800

    Delete xxt01.js

--- Scripts/xxt01.js
+++
@@ -1,8 +0,0 @@
-if ($request.method != "POST" || $response.status != 200) {
-  $done({});
-} else {
-  let obj = JSON.parse($response.body);
-  obj.cumulateHour = parseFloat($obj.cumulateHour)+1000;
-  console.log($obj.cumulateHour);
-  // $done({ body: JSON.stringify(obj) });
-}
